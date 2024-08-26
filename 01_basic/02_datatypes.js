efb8fc4c64e2a3b86c885eb2e4e93fc18f83c14b
Yuvraj Singh <yuvrajsingh.dev4@gmail.com>
2024-08-26 12:02:12 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treats all js code as newer version
+
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); //code readability should be high //Study mdn docs or ecm standards
+
+//primitive data types
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+//typeof is very important
+console.log(typeof undefined) // type is undefined
+console.log(typeof null) // null type is object
