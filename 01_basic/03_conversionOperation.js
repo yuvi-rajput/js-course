ce171c3211e182d9a3f7bf4c5fcc5c4592917b60
Yuvraj Singh <yuvrajsingh.dev4@gmail.com>
2024-09-01 04:38:50 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,16 @@
+// let score = "35";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInInt = Number(score)
+// console.log(typeof(valueInInt));
+
+// "33" => 33
+//"33abc" => NaN
+// true=> 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
