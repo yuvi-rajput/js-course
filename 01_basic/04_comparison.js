6d89851d575054a24cbc3792b9096b639401a206
Yuvraj Singh <yuvrajsingh.dev4@gmail.com>
2024-09-01 05:10:31 +0000

    Confusing Comparison Operator

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1); //here js helps to convert this string to no and then start comparison
+
+
+//These comparison will help youu to get confused so dont get confuse for the same and avoid.
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
