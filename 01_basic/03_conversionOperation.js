5ebcd6709eef1634cd021a921e8d84dd655e46e9
Yuvraj Singh <yuvrajsingh.dev4@gmail.com>
2024-09-01 04:55:02 +0000

    Conversion of One datatypes to another

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -13,4 +13,16 @@
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+// ************************** Operations ************************
+
+let value = 3
+let negVal = -value
+//console.log(negVal)
+
+let str1 = "yuvraj"
+let str2 = " singh"
+let str3 = str1 + str2
+console.log(str3)
