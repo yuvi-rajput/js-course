65b1200437b08aa456f47ecfb6b9ea81a8cb0528
Yuvraj Singh <yuvrajsingh.dev4@gmail.com>
2024-09-05 17:44:27 +0000

    Study Math and Number

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,18 @@
+const score = 400;
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length)
+
+const otherNumber = 23.549
+
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//    ********************** Math *************************
+console.log(Math.abs(-5))
+console.log(Math.round(5.4))
